22eda5829068d42f1a5d6a6c52d80e7e4dc22218
benmcmurry <benmcmurry@gmail.com>
2021-08-16 14:52:41 -0600

    the teacher view is coming along. It looks ok.

--- teacher/index.php
+++ teacher/index.php
@@ -5,7 +5,11 @@ if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROT
     $_SERVER['HTTPS'] = 'on';
 }
 
-if ($_SERVER['SERVER_NAME'] == 'localhost') {$server = $_SERVER['SERVER_NAME']."/~benmcmurry";} else {$server=" https://".$_SERVER['SERVER_NAME'];}
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
+    $server = $_SERVER['SERVER_NAME'] . "/~benmcmurry";
+} else {
+    $server = " https://" . $_SERVER['SERVER_NAME'];
+}
 
 include_once("../cas-go.php");
 include_once('../../../connectFiles/connect_ar.php');
@@ -24,6 +28,60 @@ include_once('../addUser.php');
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script type="text/javascript"></script>
+    <style>
+        .action-item {
+            /* width: 3em;
+           height: 3em; */
+            /* border-radius: 2em;  */
+
+            border: 0px;
+            /* padding: .2em .5em; */
+        }
+
+        .bi {
+            /* font-size: .3em; */
+        }
+
+        .promptList {
+            background-color: #efefef;
+            border-radius: 5px;
+            padding: 1em;
+        }
+
+        .prompt-toolbar {
+            margin: 0px;
+            padding-right: 0;
+            padding-left: 0;
+        }
+
+        .prompt-information {
+            margin:0px;
+            border: 1px solid black;
+            border-radius: .5em;
+            background-color: white;
+        }
+
+        .prompt-information p {
+            padding-top: .5em;
+            padding-bottom: .5em;
+            padding-left: 0;
+            padding-right: 0;
+            margin: 0;
+        }
+
+        .prompt-title {
+            font-size: 1rem;
+            line-height: 1.5;
+            font-weight: bold;
+
+            padding: .375rem .75rem  .375rem 0rem;
+        }
+
+        .toolbar-button {
+            padding: .375rem .375rem;
+        }
+    </style>
+
 </head>
 
 <body>
@@ -41,22 +99,35 @@ include_once('../addUser.php');
         </div>
     </header>
     <main role="main">
-        <div class="container mt-5 mb-5">
-        <?php
-                    
-                    $query = $elc_db->prepare("Select * from Prompts where netid=? and archive=0 order by date_created DESC");
-                    $query->bind_param("s", $netid);
-                    $query->execute();
-                    $result = $query->get_result();
-                    while ($row = $result->fetch_assoc()) {
-                        echo "<div class='row promptList' id='".$row['prompt_id']."'>";
-                        echo "<div><i class='bi-alarm'></i><strong>".$row['title']."</strong> <span class='small'>(last modified: ".$row['date_created'].")</span></div>";
-                        echo "<div><a href='".$server."/audioRecorder/?prompt_id=".$row['prompt_id']."'>Student Link</a></div>";
-                        echo "<div class='action_list'><a class='archive' data-promptId='".$row['prompt_id']."'>Archive</a><a class='responses' href='responses.php?prompt_id=".$row['prompt_id']."'>Edit and View Responses</a></div>";
-                        echo "</div>";
-                    }
-                    
-                ?>
+        <div class="container-sm mt-5 mb-5">
+            <?php
+
+            $query = $elc_db->prepare("Select * from Prompts where netid=? and archive=0 order by date_created DESC");
+            $query->bind_param("s", $netid);
+            $query->execute();
+            $result = $query->get_result();
+            while ($row = $result->fetch_assoc()) {
+            ?>
+                <div class='row promptList' id='<?php echo $row['prompt_id']; ?>'>
+                    <div class='row prompt-toolbar justify-content-between'>
+                        <div class='prompt-title action-item col-sm-auto text-nowrap'><?php echo $row['title']; ?></div>
+                        <div class="btn-group col-sm-auto toolbar-buttons">
+                            <button class='btn btn-outline-primary action-item toolbar-button ' title='Copy Student Link to Clipboard'><i class='bi bi-clipboard'></i></button>
+                            <a class='btn btn-outline-primary action-item toolbar-button' role='button' title='Edit Prompt' href='responses.php?prompt_id=<?php echo $row[' prompt_id']; ?>'><i class='bi bi-pencil-square'></i></a>
+                            <a class='btn btn-outline-primary action-item toolbar-button' role='button' title='View Responses' href='responses.php?prompt_id=<?php echo $row[' prompt_id']; ?>'><i class='bi bi-eye'></i></a>
+                            <button class='btn btn-outline-primary action-item toolbar-button' title='Archive Prompt' data-promptId='<?php echo $row[' prompt_id'] ?>'><i class='bi bi-archive'></i></button>
+                        </div>
+                    </div>
+
+                    <div class='row prompt-information' style='border: 1px solid black;'>
+                        <p>You have <?php echo $row['prepare_time']; ?> seconds to prepare and <?php echo $row['response_time']; ?> seconds to respond.</p>
+                        <p><Strong>Prompt: </strong> <?php echo $row['text']; ?>
+                    </div>
+                </div>
+            <?php
+            }
+
+            ?>
         </div>
     </main>
     <footer class='p-2 bg-byu-navy text-white fixed-bottom'>
