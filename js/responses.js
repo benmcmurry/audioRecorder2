265d84c90c5c00ce299de914df3cc8218fa46942
benmcmurry <benmcmurry@gmail.com>
2024-11-01 09:56:32 -0600

    case senstive mistake

--- js/responses.js
+++ js/responses.js
@@ -40,7 +40,7 @@ function playAudio(event) {
     if (currentAudio && currentAudio !==event.target) {
         console.log("Stopping current audio:", currentAudio.src);
 
-        currentAudio.Audio.pause();
+        currentAudio.audio.pause();
     }
     currentAudio = event.target;
     console.log("Playing new audio:", currentAudio.src);
