f3dfd1f0911ff509371e294f2e6130845248da19
benmcmurry <benmcmurry@gmail.com>
2025-01-30 12:34:27 -0700

    moved CAS to shared area

--- cas-go.php
+++ cas-go.php
@@ -1,8 +1,8 @@
 <?php
 // Load the settings from the central config file
-require_once 'config.php';
+require_once '../../config.php';
 // Load the CAS lib
-require_once 'CAS.php';
+require_once '../../CAS.php';
 // Enable debugging
 phpCAS::setDebug();
 // Enable verbose error messages. Disable in production!
