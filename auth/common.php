bd5ab1f6d9b4416ea6d7c23a519c8201c77e2d0c
benmcmurry <benmcmurry@gmail.com>
2026-06-10 12:35:44 -0600

    Prefer shared auth config filename

--- auth/common.php
+++ auth/common.php
@@ -78,6 +78,7 @@ function ar_server_config() {
     $config = array();
     $configPath = shared_auth_first_readable_path(array(
         getenv('SHARED_AUTH_CONFIG_PATH') ?: '',
+        dirname(__DIR__, 3) . '/shared_auth_config.php',
         dirname(__DIR__, 3) . '/google_auth_config.php',
     ));
     if ($configPath !== '') {
